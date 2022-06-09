913a29a80a6ffb4e0ff16ccfd1347903de2cf7ae
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-09 22:52:07 +0200

    query_cursor: add exec

--- ext/tree_sitter/query_cursor.c
+++ ext/tree_sitter/query_cursor.c
@@ -22,8 +22,21 @@ static VALUE query_cursor_allocate(VALUE klass) {
   return Data_Wrap_Struct(klass, NULL, query_cursor_free, cursor);
 }
 
+static VALUE query_cursor_exec(VALUE self, VALUE query, VALUE node) {
+  TSQueryCursor *cursor = value_to_query_cursor(self);
+  TSQuery *q = value_to_query(query);
+  TSNode *n = value_to_node(node);
+
+  ts_query_cursor_exec(cursor, q, *n);
+
+  return Qnil;
+}
+
 void init_query_cursor(void) {
   cQueryCursor = rb_define_class_under(mTreeSitter, "QueryCursor", rb_cObject);
 
   rb_define_alloc_func(cQueryCursor, query_cursor_allocate);
+
+  /* Class methods */
+  rb_define_method(cQueryCursor, "allocate", query_cursor_exec, 2);
 }
