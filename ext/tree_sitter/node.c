63089637c0f91c1e0efd55a63cb4330e9b838014
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-09 14:33:01 +0200

    node: add missing?

--- ext/tree_sitter/node.c
+++ ext/tree_sitter/node.c
@@ -76,6 +76,12 @@ VALUE node_is_named(VALUE self) {
   return ts_node_is_named(*node) ? Qtrue : Qfalse;
 }
 
+VALUE node_is_missing(VALUE self) {
+  TSNode *node = value_to_node(self);
+
+  return ts_node_is_missing(*node) ? Qtrue : Qfalse;
+}
+
 void init_node(void) {
   cNode = rb_define_class_under(mTreeSitter, "Node", rb_cObject);
 
@@ -90,4 +96,5 @@ void init_node(void) {
   rb_define_method(cNode, "to_s", node_string, 0);
   rb_define_method(cNode, "null?", node_is_null, 0);
   rb_define_method(cNode, "named?", node_is_null, 0);
+  rb_define_method(cNode, "missing?", node_is_missing, 0);
 }
