8496fd9b33eabf357003aa25a84ca9d1732997f8
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-10 09:07:08 +0200

    point: finish struct getters + inspect

--- ext/tree_sitter/point.c
+++ ext/tree_sitter/point.c
@@ -28,8 +28,32 @@ VALUE new_point(const TSPoint *point) {
   return res;
 }
 
+static VALUE point_row(VALUE self) {
+  TSPoint *point = value_to_point(self);
+
+  return INT2NUM(point->row);
+}
+
+static VALUE point_column(VALUE self) {
+  TSPoint *point = value_to_point(self);
+
+  return INT2NUM(point->column);
+}
+
+static VALUE point_inspect(VALUE self) {
+  TSPoint *point = value_to_point(self);
+
+  return rb_sprintf("{row=%d, column=%d}", point->row, point->column);
+}
+
 void init_point(void) {
   cPoint = rb_define_class_under(mTreeSitter, "Point", rb_cObject);
 
   rb_define_alloc_func(cPoint, point_allocate);
+
+  /* Class methods */
+  rb_define_method(cPoint, "row", point_row, 0);
+  rb_define_method(cPoint, "column", point_column, 0);
+  rb_define_method(cPoint, "inspect", point_inspect, 0);
+  rb_define_method(cPoint, "to_s", point_inspect, 0);
 }
