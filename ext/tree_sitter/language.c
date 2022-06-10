5d03ab065df8cee6a40451f31a088f27216c5651
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-10 08:00:28 +0200

    languaue: add symbol_count

--- ext/tree_sitter/language.c
+++ ext/tree_sitter/language.c
@@ -16,6 +16,14 @@ VALUE new_language(const TSLanguage *language) {
   return Data_Wrap_Struct(cLanguage, NULL, NULL, (void *)language);
 }
 
+static VALUE language_symbol_count(VALUE self) {
+  TSLanguage *language = value_to_language(self);
+  return INT2NUM(ts_language_symbol_count(language));
+}
+
 void init_language(void) {
   cLanguage = rb_define_class_under(mTreeSitter, "Language", rb_cObject);
+
+  /* Class methods */
+  rb_define_method(cLanguage, "symbol_count", language_symbol_count, 0);
 }
