9bd1d970ce6df9cb2a488a5bcdc418311796bac6
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-14 10:24:25 +0200

    query_capture: add inspect/to_s

--- ext/tree_sitter/query_capture.c
+++ ext/tree_sitter/query_capture.c
@@ -8,6 +8,12 @@ DATA_WRAP(QueryCapture, query_capture)
 DATA_DEFINE_GETTER(query_capture, index, INT2NUM)
 DATA_DEFINE_GETTER(query_capture, node, new_node_by_val)
 
+static VALUE query_capture_inspect(VALUE self) {
+  TSQueryCapture query_capture = SELF;
+  return rb_sprintf("{index=%d, node=%+" PRIsVALUE "}", query_capture.index,
+                    new_node(&query_capture.node));
+}
+
 void init_query_capture(void) {
   cQueryCapture =
       rb_define_class_under(mTreeSitter, "QueryCapture", rb_cObject);
@@ -17,4 +23,6 @@ void init_query_capture(void) {
   /* Class methods */
   DECLARE_GETTER(cQueryCapture, query_capture, index)
   DECLARE_GETTER(cQueryCapture, query_capture, node)
+  rb_define_method(cQueryCapture, "inspect", query_capture_inspect, 0);
+  rb_define_method(cQueryCapture, "to_s", query_capture_inspect, 0);
 }
