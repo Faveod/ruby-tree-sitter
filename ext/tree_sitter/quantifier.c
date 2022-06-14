ae386f8c1f871baa9722a34b0a01453d688bc5e2
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-14 11:59:07 +0200

    quantifier: add quantifier_name

--- ext/tree_sitter/quantifier.c
+++ ext/tree_sitter/quantifier.c
@@ -25,8 +25,16 @@ const char *quantifier_str(TSQuantifier error) {
   }
 }
 
+static VALUE quantifier_name(VALUE self, VALUE quant) {
+  int q = NUM2INT(quant);
+  return safe_str(quantifier_str(q));
+}
+
 void init_quantifier(void) {
   mQuantifier = rb_define_module_under(mTreeSitter, "Quantifier");
+
+  rb_define_module_function(mTreeSitter, "quantifier_name", quantifier_name, 1);
+
   rb_define_const(mQuantifier, "ZERO", INT2NUM(0));
   rb_define_const(mQuantifier, "ZERO_OR_ONE", INT2NUM(1));
   rb_define_const(mQuantifier, "ZERO_OR_MORE", INT2NUM(2));
