0a873e2ac27039ba7df3c050be00f2de0c949798
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-15 15:10:21 +0200

    test: parser: add a small program

--- test/tree_sitter/parser_test.rb
+++ test/tree_sitter/parser_test.rb
@@ -5,6 +5,14 @@ require_relative '../test_helper.rb'
 ruby = lang('ruby')
 parser = TreeSitter::Parser.new
 
+program = <<~RUBY
+def mul(a, b)
+  res = a* b
+  puts res.inspect
+  return res
+end
+RUBY
+
 describe 'loading a language' do
   it 'must set/get the same language' do
     parser.language = ruby
