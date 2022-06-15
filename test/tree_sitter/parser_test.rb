8491e4b55286c55abaddd8687f22415bad69a33d
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-15 15:37:46 +0200

    test: parser: parse_string

--- test/tree_sitter/parser_test.rb
+++ test/tree_sitter/parser_test.rb
@@ -4,14 +4,21 @@ require_relative '../test_helper.rb'
 
 ruby = lang('ruby')
 parser = TreeSitter::Parser.new
+parser.language = ruby
 
 program = <<~RUBY
-def mul(a, b)
-  res = a* b
-  puts res.inspect
-  return res
-end
+  def mul(a, b)
+    res = a* b
+    puts res.inspect
+    return res
+  end
 RUBY
+
+margorp = <<~YBUR
+  fibfast n = fib' 0 1 n
+    where fib' a b n | n <= 1 = b
+                     | otherwise = fib' b (a+b) (n-1)
+YBUR
 
 describe 'loading a language' do
   it 'must set/get the same language' do
@@ -19,3 +26,31 @@ describe 'loading a language' do
     assert_equal ruby, parser.language
   end
 end
+
+describe 'parse_string' do
+  it 'must parse nil' do
+    res = parser.parse_string(nil, nil)
+    assert_nil res
+  end
+
+  [
+    ['empty', '', 0],
+    ['valid', program, 1],
+    ['invalid', margorp, 3]
+  ].each do |q, p, c|
+    it "must parse #{q} programs" do
+      res = parser.parse_string(nil, p)
+      assert_instance_of TreeSitter::Tree, res
+
+      root = res.root_node
+      assert_instance_of TreeSitter::Node, root
+      assert_equal c, root.child_count
+    end
+  end
+end
+
+# TODO: parsing with non-nil tree.
+
+# TODO: parsing Input streams.  We're currently just hading the callback from
+#       C-space to Ruby-space At some point we might need to implement a
+#       buffered input reader and we should test it here.
