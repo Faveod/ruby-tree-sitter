f137a773cf15ae7d1ba2c4c9a2c9873aff2e4761
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-15 16:17:24 +0200

    test: parser: parse_string_encoded in utf8/utf16

--- test/tree_sitter/parser_test.rb
+++ test/tree_sitter/parser_test.rb
@@ -20,6 +20,9 @@ margorp = <<~YBUR
                      | otherwise = fib' b (a+b) (n-1)
 YBUR
 
+program_16 = program.encode('utf-16')
+margorp_16 = margorp.encode('utf-16')
+
 describe 'loading a language' do
   it 'must set/get the same language' do
     parser.language = ruby
@@ -49,6 +52,32 @@ describe 'parse_string' do
   end
 end
 
+describe 'parse_string_encoding' do
+  it 'must parse nil' do
+    res = parser.parse_string_encoding(nil, nil, :utf8)
+    assert_nil res
+    res = parser.parse_string_encoding(nil, nil, :utf16)
+    assert_nil res
+  end
+
+  [
+    ['empty', '', 0, :utf8],
+    ['valid', program, 1, :utf8],
+    ['invalid', margorp, 3, :utf8],
+    ['empty', ''.encode('utf-16'), 0, :utf16],
+    ['valid', program_16, 1, :utf16],
+    ['invalid', margorp_16, 1, :utf16]
+  ].each do |q, p, c, e|
+    it "must parse #{q} programs in #{e}" do
+      res = parser.parse_string_encoding(nil, p, e)
+      assert_instance_of TreeSitter::Tree, res
+
+      root = res.root_node
+      assert_instance_of TreeSitter::Node, root
+      assert_equal c, root.child_count
+    end
+  end
+end
 # TODO: parsing with non-nil tree.
 
 # TODO: parsing Input streams.  We're currently just hading the callback from
