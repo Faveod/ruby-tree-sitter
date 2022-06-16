068805f1e4b223db1480585a631d4a6db3d24fc1
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-16 16:18:48 +0200

    test: language: symbol for name

--- test/tree_sitter/language_test.rb
+++ test/tree_sitter/language_test.rb
@@ -6,6 +6,21 @@ ruby = lang('ruby')
 parser = TreeSitter::Parser.new
 parser.language = ruby
 
+program = <<~RUBY
+  def mul(a, b)
+    res = a* b
+    puts res.inspect
+    return res
+  end
+RUBY
+
+tree = parser.parse_string(nil, program)
+root = tree.root_node
+
+# NOTE: one should be weary of testing with data structures that are owned by
+# parsers.   They are not reliable and we should expect them to break when these
+# parsers evolve.
+
 describe 'language' do
   it 'must return symbol count' do
     assert ruby.symbol_count.positive?
@@ -13,5 +28,9 @@ describe 'language' do
 
   it 'must return symbol name' do
     assert_equal 'end', ruby.symbol_name(0)
+  end
+
+  it 'must return symbol id for string name' do
+    assert ruby.symbol_for_name(root.type, root.named?).positive?
   end
 end
