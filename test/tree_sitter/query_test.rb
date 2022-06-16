6eb74c1e6793b1afb8a53191e4ca5dcbd9bcf52d
Firas al-Khalil <firasalkhalil@gmail.com>
2022-06-16 15:07:28 +0200

    test: query: capture count

--- test/tree_sitter/query_test.rb
+++ test/tree_sitter/query_test.rb
@@ -17,3 +17,26 @@ RUBY
 tree = parser.parse_string(nil, program)
 root = tree.root_node
 
+puts root
+
+pattern = '(method_parameters)'
+capture = '(method_parameters (_)+ @args)'
+# string = '(method_parameters (_)+ @args)'
+
+# NOTE: It' still unclear to me what a captured string is.
+
+describe 'pattern/capture/string' do
+  it 'must return an Integer for pattern count' do
+    query = TreeSitter::Query.new(ruby, pattern)
+    assert_equal 1, query.pattern_count
+    assert_equal 0, query.capture_count
+    assert_equal 0, query.string_count
+  end
+
+  it 'must return an Integer for pattern count' do
+    query = TreeSitter::Query.new(ruby, capture)
+    assert_equal 1, query.pattern_count
+    assert_equal 1, query.capture_count
+    assert_equal 0, query.string_count
+  end
+end
