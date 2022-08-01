edeb10acf600a34e46b583620084b764e2cc2e96
Firas al-Khalil <firasalkhalil@gmail.com>
2022-08-01 15:47:00 +0200

    node: index access: remove unnecessary warning on SEGFAULTs

--- lib/tree_sitter/node.rb
+++ lib/tree_sitter/node.rb
@@ -22,11 +22,6 @@ module TreeSitter
     #
     # @return [Node] The named child @idx `if idx.is_a?(Numeric)`, or the named child
     # for the given field name `if idx.is_a(String) || idx.is_a?(Symbol).`
-    #
-    # @note this will explode (SEGFAULT) if you give it the wrong name. Since we
-    # don't have access to the language object to check whether the name exists
-    # (through `Language#field_id_for_name`), there's not much we can de from withing.
-    # You need to make sure that the field name you're calling exists before doing so.
     def [](idx)
       case idx
       when Numeric        then named_child(idx)
