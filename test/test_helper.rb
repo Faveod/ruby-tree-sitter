de8d44027c35027159f0304c7e73c55602419ca9
Firas al-Khalil <firasalkhalil@gmail.com>
2022-08-11 10:24:52 +0200

    test: at_exit { GC.start } at correct position

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
+at_exit { GC.start }
+
 require 'minitest/autorun'
 require 'minitest/color'
 require 'tree_sitter'
 
 require_relative '../examples/helpers'
-
-at_exit { GC.start }
