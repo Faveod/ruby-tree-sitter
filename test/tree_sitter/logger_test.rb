5bffa5c68d581d00883d75dde4567d0816cd86e1
Firas al-Khalil <firasalkhalil@gmail.com>
2022-12-30 16:42:52 +0100

    test: logging: fork for stderr logging

--- test/tree_sitter/logger_test.rb
+++ test/tree_sitter/logger_test.rb
@@ -15,6 +15,16 @@ program = <<~RUBY
   end
 RUBY
 
+# Use with fork.
+#
+# Minitest on ruby 2.7 was failing sometimes for no particular reason,
+# complaining about some random number not having a `write` method.
+#
+# My assumption is that the global var $stderr is not playing nice when
+# we try to replace it and other minitest stuff are going alongside.
+#
+# If this theory holds, then forking should solve any race conditions that
+# minitest could introduce. Let's wait and see.
 def capture_stderr
   # The output stream must be an IO-like object. In this case we capture it in
   # an in-memory IO object so we can return the string value. You can assign any
@@ -30,12 +40,14 @@ end
 
 describe 'logging' do
   it 'should log to stderr by default' do
-    captured_output = capture_stderr do
-      # Does not output anything directly.
-      parser.logger = TreeSitter::Logger.new
-      parser.parse_string(nil, program)
+    fork do
+      captured_output = capture_stderr do
+        # Does not output anything directly.
+        parser.logger = TreeSitter::Logger.new
+        parser.parse_string(nil, program)
+      end
+      refute_equal 0, captured_output.length
     end
-    refute_equal 0, captured_output.length
   end
 
   it 'should log to IO objects' do
