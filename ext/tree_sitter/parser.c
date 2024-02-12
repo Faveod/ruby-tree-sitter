e6b0410b2735192e36486337508a93ee6b6df5fc
Firas al-Khalil <firasalkhalil@gmail.com>
2024-02-12 14:17:32 +0100

    parser: update docs for 0.20.9 and sort

--- ext/tree_sitter/parser.c
+++ ext/tree_sitter/parser.c
@@ -41,52 +41,38 @@ static VALUE parser_allocate(VALUE klass) {
 }
 
 /**
- * Get the parser's current language.
- */
-static VALUE parser_get_language(VALUE self) {
-  return new_language(ts_parser_language(SELF));
-}
-
-/**
- * Get the ranges of text that the parser will include when parsing.
- *
- * @return [Array<Range>]
- */
-static VALUE parser_get_included_ranges(VALUE self) {
-  uint32_t length;
-  const TSRange *ranges = ts_parser_included_ranges(SELF, &length);
-  VALUE res = rb_ary_new_capa(length);
-  for (uint32_t i = 0; i < length; i++) {
-    rb_ary_push(res, new_range(&ranges[i]));
-  }
-  return res;
-}
-
-/**
- * Get the duration in microseconds that parsing is allowed to take.
- *
- * @return [Integer]
- */
-static VALUE parser_get_timeout_micros(VALUE self) {
-  return ULL2NUM(ts_parser_timeout_micros(SELF));
-}
-
-/**
- * Get the parser's current logger.
- *
- * @return [Logger]
- */
-static VALUE parser_get_logger(VALUE self) {
-  return new_logger_by_val(ts_parser_logger(SELF));
-}
-
-/**
  * Get the parser's current cancellation flag pointer.
  *
  * @return [Integer]
  */
 static VALUE parser_get_cancellation_flag(VALUE self) {
   return SIZET2NUM(*ts_parser_cancellation_flag(SELF));
+}
+
+/**
+ * Set the parser's current cancellation flag pointer.
+ *
+ * If a non-null pointer is assigned, then the parser will periodically read
+ * from this pointer during parsing. If it reads a non-zero value, it will
+ * halt early, returning +nil+.
+ *
+ * @see parse
+ *
+ * @note This is not well-tested in the bindings.
+ *
+ * @return nil
+ */
+static VALUE parser_set_cancellation_flag(VALUE self, VALUE flag) {
+  unwrap(self)->cancellation_flag = NUM2SIZET(flag);
+  ts_parser_set_cancellation_flag(SELF, &unwrap(self)->cancellation_flag);
+  return Qnil;
+}
+
+/**
+ * Get the parser's current language.
+ */
+static VALUE parser_get_language(VALUE self) {
+  return new_language(ts_parser_language(SELF));
 }
 
 /**
@@ -107,6 +93,21 @@ static VALUE parser_set_language(VALUE self, VALUE language) {
 }
 
 /**
+ * Get the ranges of text that the parser will include when parsing.
+ *
+ * @return [Array<Range>]
+ */
+static VALUE parser_get_included_ranges(VALUE self) {
+  uint32_t length;
+  const TSRange *ranges = ts_parser_included_ranges(SELF, &length);
+  VALUE res = rb_ary_new_capa(length);
+  for (uint32_t i = 0; i < length; i++) {
+    rb_ary_push(res, new_range(&ranges[i]));
+  }
+  return res;
+}
+
+/**
  * Set the ranges of text that the parser should include when parsing.
  *
  * By default, the parser will always include entire documents. This function
@@ -118,9 +119,10 @@ static VALUE parser_set_language(VALUE self, VALUE language) {
  * of ranges. The parser does *not* take ownership of these ranges; it copies
  * the data, so it doesn't matter how these ranges are allocated.
  *
- * If +length+ is zero, then the entire document will be parsed. Otherwise,
- * the given ranges must be ordered from earliest to latest in the document,
- * and they must not overlap. That is, the following must hold for all
+ * If +array+'s +length+ is zero, then the entire document will be parsed.
+ * Otherwise, the given ranges must be ordered from earliest to latest in the
+ * document, and they must not overlap. That is, the following must hold for
+ * all:
  *
  *   i < length - 1: ranges[i].end_byte <= ranges[i + 1].start_byte
  *
@@ -148,39 +150,12 @@ static VALUE parser_set_included_ranges(VALUE self, VALUE array) {
 }
 
 /**
- * Set the maximum duration in microseconds that parsing should be allowed to
- * take before halting.
+ * Get the parser's current logger.
  *
- * If parsing takes longer than this, it will halt early, returning +nil+.
- *
- * @see parse
- *
- * @param timeout [Integer]
- *
- * @return [nil]
+ * @return [Logger]
  */
-static VALUE parser_set_timeout_micros(VALUE self, VALUE timeout) {
-  ts_parser_set_timeout_micros(SELF, NUM2ULL(timeout));
-  return Qnil;
-}
-
-/**
- * Set the parser's current cancellation flag pointer.
- *
- * If a non-null pointer is assigned, then the parser will periodically read
- * from this pointer during parsing. If it reads a non-zero value, it will
- * halt early, returning +nil+.
- *
- * @see parse
- *
- * @note This is not well-tested in the bindings.
- *
- * @return nil
- */
-static VALUE parser_set_cancellation_flag(VALUE self, VALUE flag) {
-  unwrap(self)->cancellation_flag = NUM2SIZET(flag);
-  ts_parser_set_cancellation_flag(SELF, &unwrap(self)->cancellation_flag);
-  return Qnil;
+static VALUE parser_get_logger(VALUE self) {
+  return new_logger_by_val(ts_parser_logger(SELF));
 }
 
 /**
@@ -329,22 +304,6 @@ static VALUE parser_parse_string_encoding(VALUE self, VALUE old_tree,
 }
 
 /**
- * Instruct the parser to start the next parse from the beginning.
- *
- * If the parser previously failed because of a timeout or a cancellation, then
- * by default, it will resume where it left off on the next call to
- * {Parser#parse} or other parsing functions. If you don't want to resume,
- * and instead intend to use this parser to parse some other document, you must
- * call {Parser#reset} first.
- *
- * @return nil
- */
-static VALUE parser_reset(VALUE self) {
-  ts_parser_reset(SELF);
-  return Qnil;
-}
-
-/**
  * Set the file descriptor to which the parser should write debugging graphs
  * during parsing. The graphs are formatted in the DOT language. You may want
  * to pipe these graphs directly to a +dot(1)+ process in order to generate
@@ -370,28 +329,70 @@ static VALUE parser_print_dot_graphs(VALUE self, VALUE file) {
   return Qnil;
 }
 
+/**
+ * Instruct the parser to start the next parse from the beginning.
+ *
+ * If the parser previously failed because of a timeout or a cancellation, then
+ * by default, it will resume where it left off on the next call to
+ * {Parser#parse} or other parsing functions. If you don't want to resume,
+ * and instead intend to use this parser to parse some other document, you must
+ * call {Parser#reset} first.
+ *
+ * @return nil
+ */
+static VALUE parser_reset(VALUE self) {
+  ts_parser_reset(SELF);
+  return Qnil;
+}
+
+/**
+ * Get the duration in microseconds that parsing is allowed to take.
+ *
+ * @return [Integer]
+ */
+static VALUE parser_get_timeout_micros(VALUE self) {
+  return ULL2NUM(ts_parser_timeout_micros(SELF));
+}
+
+/**
+ * Set the maximum duration in microseconds that parsing should be allowed to
+ * take before halting.
+ *
+ * If parsing takes longer than this, it will halt early, returning +nil+.
+ *
+ * @see parse
+ *
+ * @param timeout [Integer]
+ *
+ * @return [nil]
+ */
+static VALUE parser_set_timeout_micros(VALUE self, VALUE timeout) {
+  ts_parser_set_timeout_micros(SELF, NUM2ULL(timeout));
+  return Qnil;
+}
+
 void init_parser(void) {
   cParser = rb_define_class_under(mTreeSitter, "Parser", rb_cObject);
 
   rb_define_alloc_func(cParser, parser_allocate);
 
   /* Class methods */
-  rb_define_method(cParser, "language", parser_get_language, 0);
-  rb_define_method(cParser, "language=", parser_set_language, 1);
-  rb_define_method(cParser, "included_ranges", parser_get_included_ranges, 0);
-  rb_define_method(cParser, "included_ranges=", parser_set_included_ranges, 1);
-  rb_define_method(cParser, "timeout_micros", parser_get_timeout_micros, 0);
-  rb_define_method(cParser, "timeout_micros=", parser_set_timeout_micros, 1);
-  rb_define_method(cParser, "logger", parser_get_logger, 0);
-  rb_define_method(cParser, "logger=", parser_set_logger, 1);
   rb_define_method(cParser, "cancellation_flag", parser_get_cancellation_flag,
                    0);
   rb_define_method(cParser, "cancellation_flag=", parser_set_cancellation_flag,
                    1);
+  rb_define_method(cParser, "included_ranges", parser_get_included_ranges, 0);
+  rb_define_method(cParser, "included_ranges=", parser_set_included_ranges, 1);
+  rb_define_method(cParser, "language", parser_get_language, 0);
+  rb_define_method(cParser, "language=", parser_set_language, 1);
+  rb_define_method(cParser, "logger", parser_get_logger, 0);
+  rb_define_method(cParser, "logger=", parser_set_logger, 1);
   rb_define_method(cParser, "parse", parser_parse, 2);
   rb_define_method(cParser, "parse_string", parser_parse_string, 2);
   rb_define_method(cParser, "parse_string_encoding",
                    parser_parse_string_encoding, 3);
-  rb_define_method(cParser, "reset", parser_reset, 0);
   rb_define_method(cParser, "print_dot_graphs", parser_print_dot_graphs, 1);
+  rb_define_method(cParser, "reset", parser_reset, 0);
+  rb_define_method(cParser, "timeout_micros", parser_get_timeout_micros, 0);
+  rb_define_method(cParser, "timeout_micros=", parser_set_timeout_micros, 1);
 }
