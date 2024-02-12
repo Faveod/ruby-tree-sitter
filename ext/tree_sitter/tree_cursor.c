7696177fe8d563ed9421b4da5a72f5242640006e
Firas al-Khalil <firasalkhalil@gmail.com>
2024-02-12 14:32:13 +0100

    tree_cursor: add methods from v0.20.9

--- ext/tree_sitter/tree_cursor.c
+++ ext/tree_sitter/tree_cursor.c
@@ -156,6 +156,83 @@ static VALUE tree_cursor_copy(VALUE self) {
   return res;
 }
 
+/**
+ * Re-initialize a tree cursor to the same position as another cursor.
+ *
+ * Unlike {#reset}, this will not lose parent information and allows reusing
+ * already created cursors.
+ *
+ * @return [nil]
+ */
+VALUE tree_cursor_reset_to(VALUE self, VALUE src) {
+  ts_tree_cursor_reset_to(&SELF, &unwrap(src)->data);
+  return Qnil;
+}
+
+/**
+ * Move the cursor to the previous sibling of its current node.
+ *
+ * This returns +true+ if the cursor successfully moved, and returns +false+ if
+ * there was no previous sibling node.
+ *
+ * Note, that this function may be slower than
+ * {#goto_next_sibling} due to how node positions are stored. In
+ * the worst case, this will need to iterate through all the children upto the
+ * previous sibling node to recalculate its position.
+ *
+ * @return [Boolean]
+ */
+static VALUE tree_cursor_goto_previous_sibling(VALUE self) {
+  return ts_tree_cursor_goto_previous_sibling(&SELF) ? Qtrue : Qfalse;
+}
+
+/**
+ * Move the cursor to the last child of its current node.
+ *
+ * This returns +true+ if the cursor successfully moved, and returns +false+ if
+ * there were no children.
+ *
+ * Note that this function may be slower than {#goto_first_child}
+ * because it needs to iterate through all the children to compute the child's
+ * position.
+ */
+static VALUE tree_cursor_goto_last_child(VALUE self) {
+  return ts_tree_cursor_goto_last_child(&SELF) ? Qtrue : Qfalse;
+}
+
+/**
+ * Move the cursor to the node that is the nth descendant of
+ * the original node that the cursor was constructed with, where
+ * zero represents the original node itself.
+ *
+ * @return [nil]
+ */
+static VALUE tree_cursor_goto_descendant(VALUE self, VALUE descendant_idx) {
+  uint32_t idx = NUM2UINT(descendant_idx);
+  ts_tree_cursor_goto_descendant(&SELF, idx);
+  return Qnil;
+}
+
+/**
+ * Get the index of the cursor's current node out of all of the
+ * descendants of the original node that the cursor was constructed with.
+ *
+ * @return [Integer]
+ */
+static VALUE tree_cursor_current_descendant_index(VALUE self) {
+  return UINT2NUM(ts_tree_cursor_current_descendant_index(&SELF));
+}
+
+/**
+ * Get the depth of the cursor's current node relative to the original
+ * node that the cursor was constructed with.
+ *
+ * @return [Integer]
+ */
+static VALUE tree_cursor_current_depth(VALUE self) {
+  return UINT2NUM(ts_tree_cursor_current_depth(&SELF));
+}
+
 void init_tree_cursor(void) {
   cTreeCursor = rb_define_class_under(mTreeSitter, "TreeCursor", rb_cObject);
 
@@ -164,16 +241,28 @@ void init_tree_cursor(void) {
   /* Class methods */
   rb_define_method(cTreeCursor, "initialize", tree_cursor_initialize, 1);
   rb_define_method(cTreeCursor, "reset", tree_cursor_reset, 1);
+  rb_define_method(cTreeCursor, "goto_descendant", tree_cursor_goto_descendant,
+                   1);
+  rb_define_method(cTreeCursor, "current_descendant_index",
+                   tree_cursor_current_descendant_index, 0);
+  rb_define_method(cTreeCursor, "current_depth", tree_cursor_current_depth, 0);
+  rb_define_method(cTreeCursor, "reset_to", tree_cursor_reset_to, 1);
+  rb_define_method(cTreeCursor, "goto_previous_sibling",
+                   tree_cursor_goto_previous_sibling, 0);
   rb_define_method(cTreeCursor, "current_node", tree_cursor_current_node, 0);
   rb_define_method(cTreeCursor, "current_field_name",
                    tree_cursor_current_field_name, 0);
   rb_define_method(cTreeCursor, "current_field_id",
                    tree_cursor_current_field_id, 0);
   rb_define_method(cTreeCursor, "goto_parent", tree_cursor_goto_parent, 0);
+  rb_define_method(cTreeCursor, "goto_previous_sibling",
+                   tree_cursor_goto_previous_sibling, 0);
   rb_define_method(cTreeCursor, "goto_next_sibling",
                    tree_cursor_goto_next_sibling, 0);
   rb_define_method(cTreeCursor, "goto_first_child",
                    tree_cursor_goto_first_child, 0);
+  rb_define_method(cTreeCursor, "goto_last_child", tree_cursor_goto_last_child,
+                   0);
   rb_define_method(cTreeCursor, "goto_first_child_for_byte",
                    tree_cursor_goto_first_child_for_byte, 1);
   rb_define_method(cTreeCursor, "goto_first_child_for_point",
