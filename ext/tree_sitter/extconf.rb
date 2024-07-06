e62bc9babd4a3389092fb9bb8e90329d2552353e
Firas al-Khalil <firasalkhalil@gmail.com>
2024-07-06 12:40:49 +0200

    native: support macos and x86-linux-musl
    1. tree-sitter Makefile is not prepared for corss-compilation
    2. libtree-sitter.a wasn't built correctly using ar. For some reason,
       rake-compiler or rake-compiler-doc was passing the correct CC and LD,
       but not AR.

--- ext/tree_sitter/extconf.rb
+++ ext/tree_sitter/extconf.rb
@@ -49,6 +49,7 @@ dir_include, dir_lib =
     # We need to make sure we're selecting the proper toolchain.
     # Especially needed for corss-compilation.
     ENV.store('CC', RbConfig::CONFIG['CC'])
+    repo.patch
     repo.compile
     repo.keep_static_lib
     repo.include_and_lib_dirs
